3bbccadd8b915a5238283ded9b58c6fc1443642d
Adam <adam.cierniak98@gmail.com>
2019-04-20 18:59:01 +0200

    uncompleate adding file chooser

--- ImageReceiver/ImReceiver.py
+++ ImageReceiver/ImReceiver.py
@@ -1,9 +1,29 @@
+from PIL import Image
+import numpy as np
+import logging
+import os
+#from Tkinter import Tk
+#from tkinter.filedialog import askopenfilename
+
+
 class Receiver:
 
     
 
     def __init__(self):
-        print("Receiver created")
+        logging.basicConfig(level=logging.DEBUG)
+        logging.debug("Receiver created")
+
+
+    
+    def loadPicture(self):
+        logging.debug("Your working directory is "+os.getcwd())
+        '''
+        Tk().withdraw() 
+        filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
+        print(filename)
+        '''
 
 
 s = Receiver()
+s.loadPicture()
