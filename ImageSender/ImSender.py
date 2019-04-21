15d68e7c0a8e17ad27996290afa7d595085743f7
Adam <adam.cierniak98@gmail.com>
2019-04-21 13:32:57 +0200

    sender done

--- ImageSender/ImSender.py
+++ ImageSender/ImSender.py
@@ -9,13 +9,15 @@ from tkinter.filedialog import askopenfilename
 class Sender:
 
     
-    
+    ''' Inicjalizuje logging w trybie debug'''
     def __init__(self):
         logging.basicConfig(level=logging.DEBUG)
         logging.debug("Receiver created")
+        self.img = None
+        self.numpyImg = None
 
 
-    
+    '''Laduje orazek z folderu  resources'''
     def loadPicture(self):
         logging.debug("Your working directory is "+os.getcwd())
         os.chdir(r'./resources')
@@ -23,13 +25,28 @@ class Sender:
         filename = askopenfilename() 
         logging.debug("You choosed  "+filename)
 
-        img = Image.open(filename)
-        img.load()
-        data = np.asarray( img, dtype="int32" )
-        img.show()
+        self.img = Image.open(filename)
+        self.img.load()
+
+    '''Konwertuje objekt Image na tablicę numpy'''
+    def convertImgToNumpyArray(self):
+        logging.debug("Konwersja obrazka do numpy array")
+        self.numpyImg = np.asarray( self.img, dtype="int32" )
+
+    '''Pokazuje obraz jaki został wczytany tzn przed zakłuceniami wprowadzonymi przez kanał transmisji'''
+    def show(self):
+        logging.debug("Wysylanie obrazka")
+        self.img.show() 
+
+
+    def send(self):
+        logging.debug("Wysylanie obrazka")
+        return self.numpyImg()
 
         
 
-
+# to nizej to sprawdenie  czy wszystko działa
 s = Sender()
 s.loadPicture()
+s.convertImgToNumpyArray()
+s.show()
