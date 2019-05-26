584305347643b4b1d561386b02d2c55b7321dba9
Adam <adam.cierniak98@gmail.com>
2019-05-26 14:32:15 +0200

    controler class created

--- application/controler.py
+++ application/controler.py
@@ -4,123 +4,140 @@
 from image_receiver.im_receiver  import Receiver
 from image_sender.im_sender import Sender
 from channel.channel import Channel
+import logging
+
+class Controler:
+
+    def __init__(self):
+        logging.basicConfig(level=logging.DEBUG)
+        logging.debug("Controler created")
 
 
-r = Receiver()
-s = Sender()
-c = Channel()
+    def hamming(self):
+        logging.debug("Hamming")
 
-s.load_picture()
-s.converting_to_numpy_array()
-s.show()
-
-s.flat_array()
-
-s.triple_encode()
-
-c.receive_image(s.send())
-
-c.add_noise3()
-
-r.receive_img_as_np_array(c.take_image())
-
-r.triple_decode()
-
-r.reshape()
-r.convert_numpy_array_to_image()
-r.show()
-
-'''
-******************
-***Reed Solomon***
-******************
-r = Receiver()
-s = Sender()
-c = Channel()
-
-#zaladowanie obrazka, konwersja do numpyArray i wyświetlenie obrazu przed wysłaniem
-s.load_picture()
-s.converting_to_numpy_array()
-s.show()
-
-#Tworzenie tablicy 1D
-s.flat_array()
-
-#kodowanie reeda solomona
-s.reed_solomon_encode()
+    def reed_solomon(self):
+        logging.debug("Reed Solomon")
 
 
-# wysłanie obrazka do kanał
-c.receive_image(s.send())
+        r = Receiver()
+        s = Sender()
+        c = Channel()
 
-#zauszumienie
-c.add_noise2()
+        #zaladowanie obrazka, konwersja do numpyArray i wyświetlenie obrazu przed wysłaniem
+        s.load_picture()
+        s.converting_to_numpy_array()
+        s.show()
+
+        #Tworzenie tablicy 1D
+        s.flat_array()
+
+        #kodowanie reeda solomona
+        s.reed_solomon_encode()
 
 
-#oderanie obrazka z kanału
-r.receive_img_as_np_array(c.take_image())
+        # wysłanie obrazka do kanał
+        c.receive_image(s.send())
 
-r.reed_solomon_decode()
+        #zauszumienie
+        c.add_noise2()
 
-r.reshape()
-r.convert_numpy_array_to_image()
-r.show()
+
+        #oderanie obrazka z kanału
+        r.receive_img_as_np_array(c.take_image())
+
+        r.reed_solomon_decode()
+
+        r.reshape()
+        r.convert_numpy_array_to_image()
+        r.show()
 
 
 
-
-'''
-
-'''
-*********
-***BCH***
-*********
+    def bch(self):
+        logging.debug("BCH")
 
 
-# tworzenie  glównych komponentów
-r = Receiver()
-s = Sender()
-c = Channel()
+        # tworzenie  glównych komponentów
+        r = Receiver()
+        s = Sender()
+        c = Channel()
 
-# zaladowanie obrazka, konwersja do numpyArray i wyświetlenie obrazu przed wysłaniem
-s.load_picture()
-s.converting_to_numpy_array()
-s.show()
+        # zaladowanie obrazka, konwersja do numpyArray i wyświetlenie obrazu przed wysłaniem
+        s.load_picture()
+        s.converting_to_numpy_array()
+        s.show()
 
-# Tworzenie tablicy 1D
-s.flat_array()
+        # Tworzenie tablicy 1D
+        s.flat_array()
 
-# kodowanie reeda solomona
-s.reed_solomon_encode()
+        # kodowanie reeda solomona
+        s.reed_solomon_encode()
 
-# kodowanie BCH
-# s.BCHEncode()
+        # kodowanie BCH
+        # s.BCHEncode()
 
-# wysłanie obrazka do kanał
-c.receive_image(s.send())
+        # wysłanie obrazka do kanał
+        c.receive_image(s.send())
 
-# Przywracanie wymiarów
-c.reshape()
+        # Przywracanie wymiarów
+        c.reshape()
 
-# zauszumienie
-c.add_noise()
->>>>>>> da0be4be2e251844a6843d352d43aa92c0788296
+        # zauszumienie
+        c.add_noise()
 
-# wyświetlenie zaszuumionego obazu
-c.show()
+        # wyświetlenie zaszuumionego obazu
+        c.show()
 
-# Tworzenie tablicy 1D
-c.flat_array()
+        # Tworzenie tablicy 1D
+        c.flat_array()
 
-# oderanie obrazka z kanału
-r.receive(c.take_image())
+        # oderanie obrazka z kanału
+        r.receive(c.take_image())
 
-# prostowanie tablicy
-# r.BCHDecode()
+        # prostowanie tablicy
+        # r.BCHDecode()
 
-r.reshape()
-# konwersja npyArray to obrazu i wyświetlenie go
+        r.reshape()
+        # konwersja npyArray to obrazu i wyświetlenie go
 
-r.convert_numpy_array_to_image()
-r.show()
-'''
+        r.convert_numpy_array_to_image()
+        r.show()
+
+
+    def triple(self):
+        logging.debug("Triple")
+
+        r = Receiver()
+        s = Sender()
+        c = Channel()
+
+        s.load_picture()
+        s.converting_to_numpy_array()
+        s.show()
+
+        s.flat_array()
+
+        s.triple_encode()
+
+        c.receive_image(s.send())
+
+        c.add_noise3()
+
+        r.receive_img_as_np_array(c.take_image())
+
+        r.triple_decode()
+
+        r.reshape()
+        r.convert_numpy_array_to_image()
+        r.show()
+
+
+
+
+con = Controler()
+
+#con.reed_solomon()
+con.triple()
+
+
