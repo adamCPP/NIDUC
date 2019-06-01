733edc32fa4c8a6db8a0f8d782860c040bccda5e
wamrugala <wamrugala@gmail.com>
2019-06-01 14:12:51 +0200

    Added method showing difference betweet images

--- application/statistics/statistics.py
+++ application/statistics/statistics.py
@@ -1,12 +1,23 @@
+from PIL import Image
+from PIL import ImageChops
+import logging
+
+
 class Statistics:
 
     """Inicjalizuje zmienne domyślnymi wartościami"""
     def __init__(self):
+        logging.basicConfig(level=logging.DEBUG)
+        logging.debug("Stworzenie obiektu Statistics")
         original = None
         noisy_img = None
 
     """Tworzy obraz pokazujący zmienione piksele"""
-    def difference_img(self):
+    @staticmethod
+    def difference_img(img1, img2):
+        logging.debug("Statistics: Difference image")
+        diff_img = ImageChops.difference(img1, img2)
+        diff_img.show()
         pass
 
     """Zwraca wartość zmienionych bitów w procentach"""
